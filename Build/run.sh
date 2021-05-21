28b6e19f56ce6793260b9ffab0fc60e48dff0c6a
arnanda18 <57783998+arnanda18@users.noreply.github.com>
2021-05-21 22:35:29 +0700

    Update run.sh

--- Build/run.sh
+++ Build/run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+cd apps/
+
 # clone repository
-git clone 
+git clone https://github.com/arnanda18/ci-cd-nodejs.git
 
 # run container
 docker run --name nodejs_app -p 49160:5000 -d nodeapp:latest
