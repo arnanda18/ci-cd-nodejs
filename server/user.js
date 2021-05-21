682c6750786e18cef99e7167e7eeebaa64bdbaa3
root <root@DESKTOP-H3SDEP6.localdomain>
2021-05-21 22:52:04 +0700

    update initial

--- server/user.js
+++ server/user.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-     res.send('Aplikasi berjalan pada runtime NodeJs');
+     res.send('Aplikasi berjalan pada runtime NodeJs Terbaru');
 });
 
 app.get('/admin', (req, res) => {
-    res.send('Welcome to Admin Page :)');
+    res.send('Welcome to Admin Page :) hehe');
 });
 
 app.get('/user', (req, res) => {
